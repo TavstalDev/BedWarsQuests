9c6858319c915ab0efd70dd7ffc17307b91bdaa0
tavstal <tavstal@gmail.com>
2025-09-03 02:59:21 +0200

    Comment out SnakeYAML dependency and relocation in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     // HikariCP for database connection pooling
     implementation("com.zaxxer:HikariCP:${hikariCpVersion}")
     // YAML parsing library
-    implementation("org.yaml:snakeyaml:${snakeYamlVersion}")
+    //implementation("org.yaml:snakeyaml:${snakeYamlVersion}")
     // SpiGUI for GUI creation
     implementation("com.samjakob:SpiGUI:${spiGuiVersion}")
     // Custom library for core functionality
@@ -86,7 +86,7 @@ tasks.shadowJar {
         attributes["paperweight-mappings-namespace"] = "spigot" // Add custom manifest attributes
     }
     // Relocate packages to avoid conflicts
-    relocate("org.yaml", "${projectPackageName}.shadow.snakeyaml")
+    //relocate("org.yaml", "${projectPackageName}.shadow.snakeyaml")
     relocate("com.samjakob.spigui", "${projectPackageName}.shadow.spigui")
     relocate("org.slf4j", "${projectPackageName}.shadow.slf4j")
 }
