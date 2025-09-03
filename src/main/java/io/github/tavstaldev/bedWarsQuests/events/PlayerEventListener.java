fdeb7d9a689d6179c1591c2b39b7ff3782b518f0
tavstal <tavstal@gmail.com>
2025-09-03 03:00:57 +0200

    Add daily and weekly objective assignment on player join

--- src/main/java/io/github/tavstaldev/bedWarsQuests/events/PlayerEventListener.java
+++ src/main/java/io/github/tavstaldev/bedWarsQuests/events/PlayerEventListener.java
@@ -4,6 +4,8 @@ import io.github.tavstaldev.bedWarsQuests.BedWarsQuests;
 import io.github.tavstaldev.bedWarsQuests.EventMapping;
 import io.github.tavstaldev.bedWarsQuests.managers.PlayerCacheManager;
 import io.github.tavstaldev.bedWarsQuests.models.PlayerCache;
+import io.github.tavstaldev.bedWarsQuests.models.database.DailyObjectiveData;
+import io.github.tavstaldev.bedWarsQuests.models.database.WeeklyObjectiveData;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,6 +13,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerAttemptPickupItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
+
+import java.util.Collections;
 
 public class PlayerEventListener implements Listener {
     public PlayerEventListener(Plugin plugin) {
@@ -21,6 +25,38 @@ public class PlayerEventListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         PlayerCache playerData = new PlayerCache(player);
+        if (playerData.getDailyObjectives().isEmpty()) {
+
+            var objectives = BedWarsQuests.ObjectiveManager().getObjectives();
+            if (!playerData.getWeeklyObjectives().isEmpty()) {
+                for (var weeklyObj : playerData.getWeeklyObjectives()) {
+                    objectives.removeIf(obj -> obj.Id.equals(weeklyObj.ObjectiveId));
+                }
+            }
+
+            Collections.shuffle(objectives);
+            int numToTake = Math.min(3, objectives.size());
+            for (var item : objectives.subList(0, numToTake)) {
+                BedWarsQuests.Database().AddPlayerDailyObjective(player.getUniqueId().toString(), item.Id);
+                playerData.addDailyObjective(new DailyObjectiveData(player.getUniqueId(), item.Id, false));
+            }
+        }
+
+        if (playerData.getWeeklyObjectives().isEmpty()) {
+            var objectives = BedWarsQuests.ObjectiveManager().getObjectives();
+            if (!playerData.getDailyObjectives().isEmpty()) {
+                for (var weeklyObj : playerData.getDailyObjectives()) {
+                    objectives.removeIf(obj -> obj.Id.equals(weeklyObj.ObjectiveId));
+                }
+            }
+            Collections.shuffle(objectives);
+            int numToTake = Math.min(3, objectives.size());
+            for (var item : objectives.subList(0, numToTake)) {
+                BedWarsQuests.Database().AddPlayerWeeklyObjective(player.getUniqueId().toString(), item.Id);
+                playerData.addWeeklyObjective(new WeeklyObjectiveData(player.getUniqueId(), item.Id, false));
+            }
+        }
+
         PlayerCacheManager.add(player.getUniqueId(), playerData);
     }
 
